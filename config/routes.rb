87135756bd7b7dc03cd7d4b15ba3c4db3b8c0050
EC2 Default User <ec2-user@ip-172-31-4-87.ec2.internal>
2022-03-15 09:02:57 +0000

    [Add]ルーティングresources

--- config/routes.rb
+++ config/routes.rb
@@ -1,49 +1,60 @@
 Rails.application.routes.draw do
   namespace :admin do
     get 'orders/show'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
     get 'homes/top'
+    resources :customers, only: [:index, :show, :edit]
+    resources :genres, only: [:index, :edit]
+    resources :items, only: [:index, :new, :show, :edit]
   end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
+
   namespace :public do
     get 'homes/top'
     get 'homes/about'
+    resources :addresses, only: [:index, :edit]
+    resources :orders, only: [:new, :index, :show]
+    resources :cart_items, only: [:index]
+    resources :costomers, only: [:show, :edit]
+    resources :items, only: [:index, :show]
   end
+  # namespace :admin do
+  #   get 'customers/index'
+  #   get 'customers/show'
+  #   get 'customers/edit'
+  # end
+  # namespace :admin do
+  #   get 'genres/index'
+  #   get 'genres/edit'
+  # end
+  # namespace :admin do
+  #   get 'items/index'
+  #   get 'items/new'
+  #   get 'items/show'
+  #   get 'items/edit'
+  # end
+  # namespace :admin do
+  #   get 'homes/top'
+  # end
+  # namespace :public do
+  #   get 'orders/new'
+  #   get 'orders/index'
+  #   get 'orders/show'
+  #   get 'addresses/index'
+  #   get 'addresses/edit'
+  # end
+  # namespace :public do
+  #   get 'cart_items/index'
+  # end
+  # namespace :public do
+  #   get 'customers/show'
+  #   get 'customers/edit'
+  # end
+  # namespace :public do
+  #   get 'items/index'
+  #   get 'items/show'
+  # end
+  # namespace :public do
+
+  # end
   #顧客用
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations", #登録簿

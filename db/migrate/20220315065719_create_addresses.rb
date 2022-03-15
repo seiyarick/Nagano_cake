329ce6e26df809c0d09e2b9107562c480ee54f99
EC2 Default User <ec2-user@ip-172-31-4-87.ec2.internal>
2022-03-15 07:17:22 +0000

    [Add]カラム追加

--- db/migrate/20220315065719_create_addresses.rb
+++ db/migrate/20220315065719_create_addresses.rb
@@ -3,6 +3,9 @@ class CreateAddresses < ActiveRecord::Migration[6.1]
     create_table :addresses do |t|
 
       t.timestamps
+      t.string :name #宛名
+      t.string :postal_code #郵便番号
+      t.string :address #住所
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_14_091251) do
+ActiveRecord::Schema.define(version: 2022_03_15_065937) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "postal_code"
+    t.string "address"
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +30,11 @@ ActiveRecord::Schema.define(version: 2022_03_14_091251) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "customers", force: :cascade do |t|
@@ -41,6 +54,26 @@ ActiveRecord::Schema.define(version: 2022_03_14_091251) do
     t.string "telephone_number"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "ordering_details", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end

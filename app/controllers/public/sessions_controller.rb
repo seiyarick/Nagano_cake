5f23c81140d3135262d90cc3dd5926cb3c80f0d1
EC2 Default User <ec2-user@ip-172-31-44-120.ap-northeast-1.compute.internal>
2022-03-30 08:43:03 +0000

    [update]Public会員情報編集

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -18,11 +18,19 @@ class Public::SessionsController < Devise::SessionsController
   # end
 
   protected
-  #退会しているかを判断するメソッド
+  #これからログインしてくるユーザーが退会しているかを判断するメソッド
   def customer_state
     #[処理内容１]　入力されたemailからアカウントを１件取得
     @customer = Cusomer.find_by(email: params[:customer][:email])
+    #アカウントを取得できなかった場合、このメソッドを修了する。
+    return if !@customer#もしemailがなければ
+    #[処理内容２]　取得したアカウントのパスワードと入力されたパスワードが一致しているかを判別
+    if @customer.valid_password?(params[:customer][:password]) && (@customer.is_delated==false)
+      #[処理内容]
+        redirect_to public_homes_top_path
 
+    end
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])

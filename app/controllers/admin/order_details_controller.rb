f10472f80907af861bf834cab520f31eef8f54b0
EC2 Default User <ec2-user@ip-172-31-44-120.ap-northeast-1.compute.internal>
2022-03-31 05:23:02 +0000

    [update]Adminログイン画面レイアウト

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,12 +2,14 @@ class Admin::OrderDetailsController < ApplicationController
 
   def update
     ordering_detail=OrderingDetail.find(params[:id])
+
     ordering_detail.update(ordering_detail_params)
+
     redirect_to admin_order_path(ordering_detail.id)
   end
 
   private
-  def ordering_detail
+  def ordering_detail_params
     params.require(:ordering_detail).permit(:making_status)
   end
 end
--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -29,4 +29,8 @@ class Admin::SessionsController <  Devise::SessionsController
    def after_sign_in_path_for(resouce)
      admin_homes_top_path
    end
+
+   def after_sign_out_path_for(resouce)
+     new_admin_session_path
+   end
 end

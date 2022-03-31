8b6e4dee4fe24fdef3dcfb681b30ab0a8eb4aeb7
EC2 Default User <ec2-user@ip-172-31-44-120.ap-northeast-1.compute.internal>
2022-03-31 03:33:19 +0000

    [update]Publicトップページ

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,11 +1,13 @@
 class Admin::OrderDetailsController < ApplicationController
-  
+
   def update
-  
+    ordering_detail=OrderingDetail.find(params[:id])
+    ordering_detail.update(ordering_detail_params)
+    redirect_to admin_order_path(ordering_detail.id)
   end
-  
+
   private
   def ordering_detail
     params.require(:ordering_detail).permit(:making_status)
-  end  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,17 +9,14 @@ Rails.application.routes.draw do
   }
 
   namespace :admin do
-
-
-
     get 'homes/top'
     get 'admin' => 'homes#top'
     post 'homes/top' => 'homes#top'#管理者ログイン時に通るパス
-    patch 'ordering_details/:id' => "ordering_details#update"
+    patch 'ordering_details/:id' => "order_details#update"
     resources :customers, only: [:index, :show, :edit, :update]
     resources :genres, only: [:index, :edit, :create, :update]
     resources :items, only: [:index, :new, :show, :edit, :create, :update]
-    resources :orders, only: [:show]
+    resources :orders, only: [:show, :create]
   end
 
   namespace :public do

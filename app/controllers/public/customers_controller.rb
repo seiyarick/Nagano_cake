abf2e2018bd1b47724f311237cf02672cc5d200c
EC2 Default User <ec2-user@ip-172-31-44-120.ap-northeast-1.compute.internal>
2022-04-03 13:23:36 +0000

    git commit -m

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -29,6 +29,6 @@ class Public::CustomersController < ApplicationController
   private
 
   def customer_params
-    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :email)
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
   end
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+    before_action :customer_state, only: [:create]#一番初めに行う
   # GET /resource/sign_in
   # def new
   #   super
@@ -20,14 +20,14 @@ class Public::SessionsController < Devise::SessionsController
   protected
   #これからログインしてくるユーザーが退会しているかを判断するメソッド
   def customer_state
-    #[処理内容１]　入力されたemailからアカウントを１件取得
-    @customer = Cusomer.find_by(email: params[:customer][:email])
+    #[処理内容１]　入力されたemailからアカウントを１件取得、データがあるかないか確認、なければそのままcreateを続ける
+    @customer = Customer.find_by(email: params[:customer][:email])
     #アカウントを取得できなかった場合、このメソッドを修了する。
-    return if !@customer#もしemailがなければ
-    #[処理内容２]　取得したアカウントのパスワードと入力されたパスワードが一致しているかを判別
-    if @customer.valid_password?(params[:customer][:password]) && (@customer.is_delated==true)
-      #[処理内容]
-        redirect_to public_homes_top_path
+    return if !@customer#もし@customerにemailがなければ
+    #[処理内容２]　取得したアカウントのパスワードと入力されたパスワードが一致しているか、かつ、is_deleted==true(退会している状態)ならば
+    if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted==true)
+      #[処理内容]新規登録画面に遷移。is_deletedがfalse(退会していない)ならばそのままsession/createを続行
+        redirect_to new_customer_registration_path
 
     end
   end
